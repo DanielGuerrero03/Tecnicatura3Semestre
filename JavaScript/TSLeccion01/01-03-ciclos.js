98972b8a4e442cce4e012183a0790526502aea81
Daniel Guerrero <50740422+DanielGuerrero03@users.noreply.github.com>
2023-04-24 16:27:10 -0300

    Clase 1 y 2 De javaScript
    clase Ciclos y clase Array

---
+++ JavaScript/TSLeccion01/01-03-ciclos.js
@@ -0,0 +1,42 @@
+// while
+let contador =0;
+while(contador < 3){
+    console.log(contador);
+    contador++;
+}
+console.log("Fin del ciclo while");
+
+// do while
+let conteo = 0;
+do{
+    console.log(conteo);
+    conteo++;
+}while(conteo < 3);
+console.log("Fin del ciclo do while");
+
+//for
+for(let contando = 0; contando < 3; contando++){
+    console.log(contando);
+}
+console.log("Fin del ciclo for");
+
+// Palabra reservada break
+for(let contando = 0; contando <= 10; contando++){
+    if(contando % 2 ==0){
+        console.log(contando); //Muestra todos los pares
+        break; // Al encontrar el primer numero par, rompe con el ciclo
+    }
+}
+console.log("Termina el ciclo al encontrar el primer numero par")
+
+// La palabra continue y Etiquetas Labels
+inicio:
+for(let contando = 0; contando <= 10; contando++){
+    if(contando % 2 !==0){
+        continue inicio; // Esto va a continuar a la siguiente iteración       
+    }
+    console.log(contando);
+}
+console.log("Termina el ciclo");
+
+
