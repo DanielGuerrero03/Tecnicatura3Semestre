8aad4f00ddb0b5345f65ccc3fe9c278c063565a9
Daniel Guerrero <50740422+DanielGuerrero03@users.noreply.github.com>
2023-05-27 18:06:37 -0300

    agrego funcion delete
    agrego funcion delete al crud

--- Django/bestiasbinarias/urls.py
+++ Django/bestiasbinarias/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("tareas/", include("tareas.urls"))
+    path("", include("tareas.urls"))
 ]
--- Django/tareas/views.py
+++ Django/tareas/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, redirect
 from .models import Tareas
+from .forms import TareasForm
 
 # Create your views here.
 def listar_tareas(request):
     tareas = Tareas.objects.all()
     return render(request, 'listar_tareas.html', {"tareas": tareas})
 
+
 def crear_tareas(request):
     print(request.POST)
-    tarea= Tareas(titulo=request.POST["Tarea"], descripcion=request.POST["descripcion"])
+    tarea = Tareas(titulo=request.POST["Tarea"], descripcion=request.POST["descripcion"])
     tarea.save()
-    return redirect('/tareas/')
+    return redirect('tareas')
+
+
 def eliminar_tareas(request, tareas_id):
     eliminar_tarea = Tareas.objects.get(id=tareas_id)
     eliminar_tarea.delete()
-    return redirect('/tareas/')
+    return redirect('tareas')
+
+
+def editar_tareas(request, tareas_id):
+    tarea = Tareas.objects.get(id=tareas_id)
+    forms = TareasForm(request.POST or None, instance=tarea)
+    if forms.is_valid() and request.POST:
+        forms.save()
+        return redirect('tareas')
+    return render(request, 'editar_tareas.html', {'forms':forms})
+
