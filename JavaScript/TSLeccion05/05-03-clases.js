e3e05e319080e188bb7cdb36ecd45aba84760bab
Daniel Guerrero <50740422+DanielGuerrero03@users.noreply.github.com>
2023-06-08 21:56:47 -0300

    Clase 7 JavaScrip
    Metodo Static

--- JavaScript/TSLeccion05/05-03-clases.js
+++ JavaScript/TSLeccion05/05-03-clases.js
@@ -1,7 +1,26 @@
+//let persona3 = new Persona('Karla', 'Juarez'); esto no se puede hacer porque no se puede crear un objeto de una clase abstracta           
+
+
 class Persona{ //Clase padre
+
+    static contadorPersonas = 0; //atributo estatico
+    //email = 'Valor default email'; //atributo no estatico
+
+    static get MAX_OBJ(){ //Este metodo simula una constante
+        return 5;
+    }
+
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
+        if(Persona.contadorPersonas < Persona.MAX_OBJ){
+            this.idPersona = ++Persona.contadorPersonas;    
+        }
+        else{
+            console.log('Se ha superado el maximo de objetos permitidos');
+        }
+        
+        //console.log('Se incrementa contador: '+Persona.contadorObjetosPersona);
     }
     
     get nombre(){
@@ -21,13 +40,20 @@ class Persona{ //Clase padre
     }
 
     nombreCompleto(){
-        return this._nombre+' '+this._apellido;
+        return this.idPersona+' '+this._nombre+' '+this._apellido;
     }
 
     toString(){ // Regresa un String
         return this.nombreCompleto();    
     }
 
+    static saludar(){
+        console.log('Saludos desde metodo static');
+    }
+
+    static saludar2(persona){
+        console.log(persona.nombre+' '+persona.apellido);
+    }
 }
 
 class Empleado extends Persona { //Clase hija
@@ -48,6 +74,7 @@ class Empleado extends Persona { //Clase hija
     nombreCompleto(){
         return super.nombreCompleto()+', '+this._departamento;
     } 
+
 }
 
 let persona1 = new Persona('Martin', 'Perez');
@@ -69,11 +96,40 @@ persona2.apellido = 'Garcia';
 console.log(persona2.apellido)
 //console.log(persona2);
 
-let Empleado1 = new Empleado('Maria', 'Gimenez', 'Sistemas');
-console.log(Empleado1);
-console.log(Empleado1.nombre);
-console.log(Empleado1.nombreCompleto());
+let empleado1 = new Empleado('Maria', 'Gimenez', 'Sistemas');
+console.log(empleado1);
+console.log(empleado1.nombre);
+console.log(empleado1.nombreCompleto());
 
-//Object.prototype.toString Esta la manera de acceder a atributos y metodos de manera dinamica
+//Object.proeotype.toString Esta la manera de acceder a atributos y metodos de manera dinamica
+console.log(empleado1.toString());
+console.log(persona1.toString());
 
+//persona1.saludar(); no se utiliza desde el objeto sino desde la clase
+Persona.saludar();
+Persona.saludar2(persona1);
 
+Empleado.saludar()
+Empleado.saludar2(empleado1);
+
+//console.log(persona1.contadorObjetosPersona);
+console.log(Persona.contadorObjetosPersona);
+console.log(Empleado.contadorObjetosPersona);
+
+console.log(persona1.email);
+console.log(empleado1.email);
+//console.log(Persona.email);
+console.log(persona1.toString());
+console.log(persona2.toString());
+console.log(empleado1.toString());
+console.log(Persona.contadorPersonas);
+let persona3 = new Persona('Carla', 'Pertosi');
+console.log(persona3.toString());
+console.log(Persona.contadorPersonas);
+//Persona.MAX_OBJ = 10; //No se puede modificar una constante
+console.log(Persona.MAX_OBJ);
+
+let persona4 = new Persona('Franco', 'Diaz');
+console.log(persona4.toString());
+let persona5 = new Persona('Liliana', 'Paz');
+console.log(persona5.toString());
